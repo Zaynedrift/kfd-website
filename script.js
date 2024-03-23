530a975bded2f2faa7e1f0c2b55ee8cd5beb85c8
Zaynedrift <23094889-Zaynedrift@users.noreply.replit.com>
2024-03-23 01:24:01 +0000

    moved css as style div to index.html because my website for some reason doesn't pick it up :(

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("fr").addEventListener("click", function() {
-  window.location.href = "/fr/";
-});
-document.getElementById("en").addEventListener("click", function() {
-  window.location.href = "/en/";
-});
