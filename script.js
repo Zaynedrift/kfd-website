d464c4be69ed0eda27e1f8131287447d4ebddbf7
Zaynedrift <146078609+Zaynedrift@users.noreply.github.com>
2024-03-23 01:20:40 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("fr").addEventListener("click", function() {
+  window.location.href = "/fr/";
+});
+document.getElementById("en").addEventListener("click", function() {
+  window.location.href = "/en/";
+});
